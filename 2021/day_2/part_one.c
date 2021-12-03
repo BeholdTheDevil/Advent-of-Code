ce589d39b65bbf0160fd69dfe8a44c82e7385f37
Anton Degerfeldt <antdeg@outlook.com>
2021-12-03 14:05:24 +0100

    Day two finished.

---
+++ 2021/day_2/part_one.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Calculate the horizontal position and depth you would have after following the planned
+ * course. What do you get if you multiply your final horizontal position by your final
+ * depth?
+ */
+
+int main(int argc, char *argv[])
+{
+	if(argc <= 1) {
+		fprintf(stderr, "Usage: %s <inputfile> <optional outputfile>\n", argv[0]);
+		return 1;
+	}
+
+	FILE *fp = fopen(argv[1], "r");
+
+	int hori = 0;
+	int depth = 0;
+	int temp = 0;
+	char direc[8];
+
+	while(fscanf(fp, "%s %d\n", direc, &temp) == 2) {
+		if(strncmp(direc, "up", 2) == 0) {
+			depth -= temp;
+		}
+		if(strncmp(direc, "down", 4) == 0) {
+			depth += temp;
+		}
+		if(strncmp(direc, "forward", 7) == 0) {
+			hori += temp;
+		}
+	}
+
+	printf("%d\n", hori * depth);
+
+	fclose(fp);
+	return 0;
+}
---
+++ 2021/template.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	if(argc <= 1) {
+		fprintf(stderr, "Usage: %s <inputfile> <optional outputfile>\n", argv[0]);
+		return 1;
+	}
+
+	FILE *fp = fopen(argv[1], "r");
+
+	
+
+	fclose(fp);
+	return 0;
+}
